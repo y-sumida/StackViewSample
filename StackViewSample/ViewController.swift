bea2d025328f3b6b32503ec8c4543e75dfcb8288
Yuki Sumida <y.sumida.0201@gmail.com>
2017-03-21 22:41:19 +0900

    UIStackView の Distribution を Equql Spacingに変更

--- StackViewSample/ViewController.swift
+++ StackViewSample/ViewController.swift
@@ -10,28 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var viewA: ViewA!
-    @IBOutlet weak var viewB: UIView!
-    @IBOutlet weak var bLabel: UILabel!
-    @IBOutlet weak var viewC: UIView!
-
-    //@IBOutlet weak var alabel: UILabel!
-    @IBOutlet weak var cLabel: UILabel!
+    @IBOutlet weak var viewB: ViewA!
+    @IBOutlet weak var viewC: ViewA!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //alabel.preferredMaxLayoutWidth = self.view.frame.width
-        bLabel.preferredMaxLayoutWidth = self.view.frame.width
-        cLabel.preferredMaxLayoutWidth = self.view.frame.width
-
-        //alabel.text = "aLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabelaLabel"
-
-        bLabel.text = "bLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabelbLabel"
-
-        cLabel.text = "cLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabelcLabel"
-        viewB.hidden = true
 
         viewA.label.text = "ViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabelViewALabel"
+        viewB.label.text = "BBBBBB"
+        viewB.backgroundColor = UIColor.blueColor()
+        viewC.label.text = "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"
+        viewC.backgroundColor = UIColor.redColor()
+
         viewA.label.preferredMaxLayoutWidth = self.view.frame.width
+        viewB.label.preferredMaxLayoutWidth = self.view.frame.width
+        viewC.label.preferredMaxLayoutWidth = self.view.frame.width
     }
 
     override func didReceiveMemoryWarning() {
