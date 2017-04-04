387dd23e9235691454b7aed4a75328c0395b5053
Yuki Sumida <y.sumida.0201@gmail.com>
2017-04-05 00:58:37 +0900

    Swift3対応

--- StackViewSample/ViewA.swift
+++ StackViewSample/ViewA.swift
@@ -22,21 +22,21 @@ class ViewA: UIView {
     }
 
     func loadView() {
-        let className = NSStringFromClass(self.dynamicType)
-        let bundle = NSBundle(forClass: self.dynamicType)
-        let nibName = className.componentsSeparatedByString(".").last!
+        let className = NSStringFromClass(type(of: self))
+        let bundle = Bundle(for: type(of: self))
+        let nibName = className.components(separatedBy: ".").last!
         let nib = UINib(nibName: nibName, bundle: bundle)
-        let view = nib.instantiateWithOwner(self, options: nil).first as! UIView
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
         addSubview(view)
         
         // カスタムViewのサイズを自分自身と同じサイズにする
         view.translatesAutoresizingMaskIntoConstraints = false
         let bindings = ["view": view]
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|",
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|",
             options:NSLayoutFormatOptions(rawValue: 0),
             metrics:nil,
             views: bindings))
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|",
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|",
             options:NSLayoutFormatOptions(rawValue: 0),
             metrics:nil,
             views: bindings))
